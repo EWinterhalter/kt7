6348c2d27636dbdefe33827cd3a7110c2d2cbe51
ellie <bulanovaEN22@st.ithub.ru>
2025-03-07 17:24:10 +0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -9,56 +9,122 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Счетчик',
-      theme: ThemeData(primarySwatch: Colors.pink),
-      home: CounterScreen(),
+      debugShowCheckedModeBanner: false,
+      home: AuthCheck(),
     );
   }
 }
 
-class CounterScreen extends StatefulWidget {
+class AuthCheck extends StatefulWidget {
   @override
-  _CounterScreenState createState() => _CounterScreenState();
+  _AuthCheckState createState() => _AuthCheckState();
 }
 
-class _CounterScreenState extends State<CounterScreen> {
-  int _counter = 0;
+class _AuthCheckState extends State<AuthCheck> {
+  bool _isLoggedIn = false;
 
   @override
   void initState() {
     super.initState();
-    _loadCounter();
+    _checkLoginStatus();
   }
 
-  Future<void> _loadCounter() async {
-    final prefs = await SharedPreferences.getInstance();
+  Future<void> _checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      _counter = prefs.getInt('counter') ?? 0;
-    });
-  }
-
-  Future<void> _incrementCounter() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _counter++;
-      prefs.setInt('counter', _counter);
+      _isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return _isLoggedIn ? HomeScreen() : LoginScreen();
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  Future<void> _login() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', _nameController.text);
+    await prefs.setBool('isLoggedIn', true);
+    setState(() {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Счетчик')),
+      appBar: AppBar(title: Text('Авторизация')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Введите ник'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Войти'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username') ?? 'Гость';
+    });
+  }
+
+  Future<void> _logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', false);
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Главная')), 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Количество нажатий:', style: TextStyle(fontSize: 20)),
-            Text('$_counter', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+          children: [
+            Text('Добро пожаловать, $_username!', style: TextStyle(fontSize: 20)),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('+'),
+              onPressed: _logout,
+              child: Text('Выйти'),
             ),
           ],
         ),
