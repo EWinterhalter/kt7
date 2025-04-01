33840e7a441344ec72c682fb9d4735b3c4bd927e
ellie <bulanovaEN22@st.ithub.ru>
2025-04-01 11:22:51 +0300

    aga

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,126 +9,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthCheck(),
+      title: 'КТ 7',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
 
-class AuthCheck extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _AuthCheckState createState() => _AuthCheckState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _AuthCheckState extends State<AuthCheck> {
-  bool _isLoggedIn = false;
+class _HomePageState extends State<HomePage> {
+  final Dio _dio = Dio();
+  List<dynamic> _data = [];
 
   @override
   void initState() {
     super.initState();
-    _checkLoginStatus();
+    fetchData();
   }
 
-  Future<void> _checkLoginStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _isLoggedIn ? HomeScreen() : LoginScreen();
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _nameController = TextEditingController();
-
-  Future<void> _login() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('username', _nameController.text);
-    await prefs.setBool('isLoggedIn', true);
-    setState(() {});
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+  Future<void> fetchData() async {
+    try {
+      final response = await _dio.get('https://dummyjson.com/posts');
+      setState(() {
+        _data = response.data['posts'];
+      });
+    } catch (e) {
+      print('Ошибка: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Авторизация')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Введите ник'),
+      appBar: AppBar(title: Text('Посты')),
+      body: _data.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _data.length,
+              itemBuilder: (context, index) {
+                final item = _data[index];
+                return ListTile(
+                  title: Text(item['title']),
+                  subtitle: Text(item['body']),
+                );
+              },
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Войти'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String _username = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserData();
-  }
-
-  Future<void> _loadUserData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _username = prefs.getString('username') ?? 'Гость';
-    });
-  }
-
-  Future<void> _logout() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isLoggedIn', false);
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => LoginScreen()));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Главная')), 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Добро пожаловать, $_username!', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _logout,
-              child: Text('Выйти'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
